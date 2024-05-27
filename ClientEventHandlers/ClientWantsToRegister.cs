0fa2f681ba5f39dc93d8b335a6ba64b02818ebf0
Miran <112928970+Mcfearless314@users.noreply.github.com>
2024-05-27 12:00:28 +0200

    Update ClientWantsToRegister.cs

--- ClientEventHandlers/ClientWantsToRegister.cs
+++ ClientEventHandlers/ClientWantsToRegister.cs
@@ -7,9 +7,10 @@ namespace Backend.ClientEventHandlers;
 
 public class ClientWantsToRegisterDto : BaseDto
 {
+    public string Email { get; set; }
     public string Username { get; set; }
     public string Password { get; set; }
-    public string Email { get; set; }
+
 }
 
 public class ClientWantsToRegister : BaseEventHandler<ClientWantsToRegisterDto>
